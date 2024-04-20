8faa70f282b18b9eb1e39b894ae8695f5b26fbb2
Shakti Kumar <shakti.opensourcedeveloper@gmail.com>
2024-04-20 15:29:36 +0530

    Micro Frontend react

---
+++ home/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>Micro Frontend Footer</div>
+  )
+}
+
+export default Footer
---
+++ home/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Micro Frontend Header</div>
+  )
+}
+
+export default Header
